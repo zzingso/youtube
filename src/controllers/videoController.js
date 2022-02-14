4fb751303e5c2b659b3c9d2d7fc891c7914f27a2
zzingso <ravie90@gmail.com>
2022-02-14 21:41:39 +0900

    feat : Learn the features of PUG

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,48 @@
-export const trending = (req, res) => res.render("home", { pageTitle : "Home" });
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+const videos = [
+  {
+    title : "Hello",
+    rating : 5,
+    comments : 2,
+    createdAt : "2 minutes age",
+    views : 59,
+    id : 1,
+  },
+  {
+    title : "Hello2",
+    rating : 5,
+    comments : 2,
+    createdAt : "2 minutes age",
+    views : 59,
+    id : 2,
+  },
+  {
+    title : "Hello3",
+    rating : 5,
+    comments : 2,
+    createdAt : "2 minutes age",
+    views : 59,
+    id : 3,
+  }
+];
+
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle : "Home", videos });
+};
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id-1];
+  return res.render("watch", { pageTitle : `Watching: ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id-1];
+  return res.render("edit", { pageTitle : `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  return res.redirect(`/videos/${id}`);
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
