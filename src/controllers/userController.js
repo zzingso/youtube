b003f20beb11ac322c28ee46818760b313afa204
zzingso <ravie90@gmail.com>
2022-03-03 16:30:51 +0900

    Feat : My profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -252,4 +252,12 @@ export const postChangePassword = async (req, res) => {
   await user.save();
   return res.redirect("/users/logout");
 };
-export const see = (req, res) => res.send("See User");
+
+export const see = (req, res) => {
+  const { id } = req.params;
+  const user = User.findById(id);
+  if (!user) {
+    return res.status(404).render("404", { pageTitle: "User not found." });
+  }
+  return res.render("profile", { pageTitle: user.name, user });
+};
